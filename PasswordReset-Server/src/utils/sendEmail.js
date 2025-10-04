e312c8888f429d17f10f461eb2fdb0afaca68093
amarnath-98 <amarmagesh717@gmail.com>
2025-10-04 15:51:15 +0530

    same problem

--- PasswordReset-Server/src/utils/sendEmail.js
+++ PasswordReset-Server/src/utils/sendEmail.js
@@ -1,27 +1,48 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, 
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Recipe App" <${process.env.EMAIL_USER}>`,
+    const msg = {
       to,
+      from: process.env.SENDGRID_SENDER, // must be a verified sender
       subject,
       html,
-    });
+    };
 
+    await sgMail.send(msg);
     console.log("Email sent to:", to);
   } catch (err) {
-    console.error("Email sending failed:", err.message);
+    console.error("SendGrid email failed:", err.response?.body || err.message);
     throw err;
   }
 };
+
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (to, subject, html) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     await transporter.sendMail({
+//       from: `"Recipe App" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+
+//     console.log("Email sent to:", to);
+//   } catch (err) {
+//     console.error("Email sending failed:", err.message);
+//     throw err;
+//   }
+// };
